c21d6f954ade878938b58f47c01091c83c1a5971
Abdul Rehman <adnan1470369258@gmail.com>
2022-07-07 15:37:11 +0500

    updated

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hdwmbb975yszo#03*hc+o0=1_z=2b#9tv^8nue6b*c&(7l(0tn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "https://carvanadmin.herokuapp.com", "http://carvanadmin.herokuapp.com"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "https://carvanadmin.herokuapp.com", "http://carvanadmin.herokuapp.com", "carvanadmin.herokuapp.com'"]
 CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', "https://carvanadmin.herokuapp.com", "http://carvanadmin.herokuapp.com"]
 
 # Application definition
